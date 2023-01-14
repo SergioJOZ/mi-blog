c51249109d37ffb232e17d0f2fbda89f400b83f8
SegioJOZ <sergio2jo13@gmail.com>
2023-01-14 19:38:38 -0400

    Changed params on connection

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     //Paramteros dentro de objeto //SOLO EN CASO DE AVISO
     //useNewUrlParser : true
     //useUnifiedTopology : true
