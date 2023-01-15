06244b749b5272d14bb3ff0b7db8d7d783582d06
SegioJOZ <sergio2jo13@gmail.com>
2023-01-14 21:12:43 -0400

    Modified global url 3

--- client/helpers/Global.jsx
+++ client/helpers/Global.jsx
@@ -1,6 +1,1 @@
-import axios from "axios";
-
-export const Global =
-  axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-  }) + "/api/";
+export const Global = "https://miblog-api.onrender.com" + "/api/";
