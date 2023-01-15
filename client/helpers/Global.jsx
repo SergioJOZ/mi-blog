919971513ceaa17a0f8dfe9d7600032c3e2702a5
SegioJOZ <sergio2jo13@gmail.com>
2023-01-14 20:59:17 -0400

    Modified global url 2

--- client/helpers/Global.jsx
+++ client/helpers/Global.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-export const Global = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
+export const Global =
+  axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+  }) + "/api/";
